44bc4dbe055d99b73bac3615b54c5337bd9407e1
Jonathan <839560084@qq.com>
2017-07-17 14:08:00 +0800

    数据库config.php修改

--- Application/Common/Conf/config2.php
+++ Application/Common/Conf/config2.php
@@ -22,7 +22,7 @@ return array(
 //    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
 //    'DB_NAME'               =>  'zhongchou',          // 数据库名
 //    'DB_USER'               =>  'root',      // 用户名
-//    'DB_PWD'                =>  'xE6hncZJAg2c#TXa',          // 密码
+//    'DB_PWD'                =>  '9MxvHrlSkoTEJF6X',          // 密码
 //    'DB_PORT'               =>  '3306',        // 端口
 //    'DB_PREFIX'             =>  'think_',    // 数据库表前缀
 //
